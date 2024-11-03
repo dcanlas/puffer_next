7d514b83f1d46643f55474a7ee9f9702f198415f
Jonathan Howland <jhowland82@gmail.com>
2024-11-03 01:47:05 -0700

    added "today" logic into DatePicker

--- components/elements/DatePicker.js
+++ components/elements/DatePicker.js
@@ -27,10 +27,18 @@ const styles = {
   },
 };
 
+const today = dayjs();
+
 export default function DatePicker(props) {
   const { defaultValue, ...rest } = props;
 
   return (
-    <DatePickerMUI defaultValue={dayjs(defaultValue)} sx={styles} {...rest} />
+    <DatePickerMUI
+      defaultValue={defaultValue === "today" ? today : dayjs(defaultValue)}
+      sx={styles}
+      {...rest}
+    />
   );
 }
+
+export { today };
